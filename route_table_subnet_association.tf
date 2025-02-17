85c4bf29a711e5b57555d0decf9c8c1ab776e2de
yashdarekar94 <darekar.y@northeastern.edu>
2025-02-17 16:02:46 -0500

    Now uploading fixed code and should run without any issues

--- route_table_subnet_association.tf
+++ route_table_subnet_association.tf
@@ -1,6 +1,6 @@
 # Private route table association
 resource "aws_route_table_association" "private_route_table_association" {
-  count = length(var.private_subnet_cider_blocks)
+  count          = length(var.private_subnet_cider_blocks)
   subnet_id      = element(aws_subnet.private_subnets[*].id, count.index)
   route_table_id = aws_route_table.private_route_table.id
 }
