f238831b0fdfedd69ea0f0b3b6d2c763312d3823
Yash Darekar <darekar.y@northeastern.edu>
2025-02-24 16:33:50 -0500

    Update gateway.tf

--- gateway.tf
+++ gateway.tf
@@ -1,6 +1,6 @@
 # Creating internet gateway
 resource "aws_internet_gateway" "my_internet_gateway" {
-  vpc_id = aws_vpc.my_vpc.id
+      vpc_id = aws_vpc.my_vpc.id
 
   tags = {
     Name = var.internet_gateway_name
