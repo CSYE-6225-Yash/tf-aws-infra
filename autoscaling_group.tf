dc88bbedbd8af0186c4bdb72590737a3d6fa2d6b
yashdarekar94 <darekar.y@northeastern.edu>
2025-04-02 15:17:05 -0400

    Fixed instances not launching in different subnets

--- autoscaling_group.tf
+++ autoscaling_group.tf
@@ -1,11 +1,12 @@
 # Creating autoscaling group
 resource "aws_autoscaling_group" "webapp_autoscaling_group" {
-  name              = var.autoscaling_group_name
-  default_cooldown  = var.autoscaling_group_cooldown
-  min_size          = var.autoscalling_group_min_size
-  max_size          = var.autoscalling_group_max_size
-  desired_capacity  = var.autoscalling_group_desired_capacity
-  target_group_arns = [aws_lb_target_group.webapp_target_group.arn]
+  name                = var.autoscaling_group_name
+  default_cooldown    = var.autoscaling_group_cooldown
+  min_size            = var.autoscalling_group_min_size
+  max_size            = var.autoscalling_group_max_size
+  desired_capacity    = var.autoscalling_group_desired_capacity
+  target_group_arns   = [aws_lb_target_group.webapp_target_group.arn]
+  vpc_zone_identifier = aws_subnet.public_subnets[*].id
 
   launch_template {
     id = aws_launch_template.webapp_launch_template.id
